af23a0e30f11b809c395cbcdedc5d887f8025a5c
ming20100 <2720387088@qq.com>
2020-02-04 23:08:35 +0800

    路由、路由传递参数

---
+++ 3. 进阶-VUE脚手架搭建项目和组件化示例/4.1-vue-router-params/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div class="nav">
+      <router-link to="/hello">hello组件</router-link>
+      <router-link to="/mycpn">MyCpn组件</router-link>
+      <router-link :to="{path: '/param108', query: { id: 123}}">参数1</router-link>
+      <router-link to="/param109/8888">参数2</router-link>
+      <router-link :to="{ name: 'param1010', params: { id: 123,msg:'你好，hello！'}}">参数3</router-link>
+
+      <!--多参数-->
+      <router-link  :to="{path: '/param1012', query: { id: 123, msg: '张三->多参数1'}}">多参数1</router-link>
+      <router-link  :to="{ name: 'param1013', params: { id: 123,msg:'你好->多参数2'}}">多参数2</router-link>
+
+    </div>
+    <div class="content">
+      <keep-alive>
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+<style scoped>
+  .nav a{
+    margin: 2px 5px;
+  }
+  .content{
+
+  }
+</style>
